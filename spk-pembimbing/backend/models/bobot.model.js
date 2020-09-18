83aa37df8d7c54851bca5f8cc04932c06453de5b
ikigalang <galang.sukma21@gmail.com>
2020-09-18 16:03:53 +0700

    update bobot untuk pembimbing 1 dan 2

--- spk-pembimbing/backend/models/bobot.model.js
+++ spk-pembimbing/backend/models/bobot.model.js
@@ -4,21 +4,41 @@ const Schema = mongoose.Schema;
 
 const bobotSchema = new Schema(
   {
-    pendidikan: {
-      type: Number,
-      required: true,
+    pembimbing1: {
+      pendidikan: {
+        type: Number,
+        required: true,
+      },
+      fungsional: {
+        type: Number,
+        required: true,
+      },
+      kompetensi: {
+        type: Number,
+        required: true,
+      },
+      kuota: {
+        type: Number,
+        required: true,
+      },
     },
-    fungsional: {
-      type: Number,
-      required: true,
-    },
-    kompetensi: {
-      type: Number,
-      required: true,
-    },
-    kuota: {
-      type: Number,
-      required: true,
+    pembimbing2: {
+      pendidikan: {
+        type: Number,
+        required: true,
+      },
+      fungsional: {
+        type: Number,
+        required: true,
+      },
+      kompetensi: {
+        type: Number,
+        required: true,
+      },
+      kuota: {
+        type: Number,
+        required: true,
+      },
     },
   },
   {
--- spk-pembimbing/backend/routes/bobot.js
+++ spk-pembimbing/backend/routes/bobot.js
@@ -10,16 +10,29 @@ router.route("/").get((req, res) => {
 
 // add new bobot
 router.route("/add").post((req, res) => {
-  const pendidikan = req.body.pendidikan;
-  const fungsional = req.body.fungsional;
-  const kompetensi = req.body.kompetensi;
-  const kuota = req.body.kuota;
+  const pendidikan1 = req.body.pembimbing1.pendidikan;
+  const fungsional1 = req.body.pembimbing1.fungsional;
+  const kompetensi1 = req.body.pembimbing1.kompetensi;
+  const kuota1 = req.body.pembimbing1.kuota;
+  const pendidikan2 = req.body.pembimbing2.pendidikan;
+  const fungsional2 = req.body.pembimbing2.fungsional;
+  const kompetensi2 = req.body.pembimbing2.kompetensi;
+  const kuota2 = req.body.pembimbing2.kuota;
+  console.log(pendidikan1);
 
   const newBobot = new Bobot({
-    pendidikan: pendidikan,
-    fungsional: fungsional,
-    kompetensi: kompetensi,
-    kuota: kuota,
+    pembimbing1: {
+      pendidikan: pendidikan1,
+      fungsional: fungsional1,
+      kompetensi: kompetensi1,
+      kuota: kuota1,
+    },
+    pembimbing2: {
+      pendidikan: pendidikan2,
+      fungsional: fungsional2,
+      kompetensi: kompetensi2,
+      kuota: kuota2,
+    },
   });
 
   newBobot
@@ -31,10 +44,14 @@ router.route("/add").post((req, res) => {
 // update bobot
 router.route("/update/:id").post((req, res) => {
   Bobot.findById(req.params.id).then((bobot) => {
-    bobot.pendidikan = req.body.pendidikan;
-    bobot.fungsional = req.body.fungsional;
-    bobot.kompetensi = req.body.kompetensi;
-    bobot.kuota = req.body.kuota;
+    bobot.pembimbing1.pendidikan = req.body.pembimbing1.pendidikan;
+    bobot.pembimbing1.fungsional = req.body.pembimbing1.fungsional;
+    bobot.pembimbing1.kompetensi = req.body.pembimbing1.kompetensi;
+    bobot.pembimbing1.kuota = req.body.pembimbing1.kuota;
+    bobot.pembimbing2.pendidikan = req.body.pembimbing2.pendidikan;
+    bobot.pembimbing2.fungsional = req.body.pembimbing2.fungsional;
+    bobot.pembimbing2.kompetensi = req.body.pembimbing2.kompetensi;
+    bobot.pembimbing2.kuota = req.body.pembimbing2.kuota;
 
     bobot
       .save()
