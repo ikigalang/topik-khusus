a655918589371fd24594a741e749e319a4360619
ikigalang <galang.sukma21@gmail.com>
2020-09-28 15:24:46 +0700

    fix login bug

--- spk-pembimbing/frontend/src/components/navbar.component.js
+++ spk-pembimbing/frontend/src/components/navbar.component.js
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
+    let address = "/home";
+    if (localStorage.getItem("loginState") === "0") {
+      address = "/login";
+    }
     return (
       <div>
         <nav className="navbar navbar-dark bg-primary navbar-expand-sm">
-          <Link to="/home" className="navbar-brand">
+          <Link to={address} className="navbar-brand">
             Sistem Pendukung Keputusan
           </Link>
         </nav>
